a8a7f9d1752a6fa9f9f092883580969c2eb50c4e
SJ Park <58803746+sakurei22@users.noreply.github.com>
2020-05-07 10:12:23 +0900

    Update Studentex.java

--- java0/1.javaBasic/src/day02/Studentex.java
+++ java0/1.javaBasic/src/day02/Studentex.java
@@ -2,7 +2,7 @@ package day02;
 // 필드의 값이 같으면 같은 객체
 public class Studentex {
 	public static void main(String[] args) {
-		Student s = new Student("신종하", 17, 90, 80, 70, "991111-1111111");
+		Student s = new Student("박수진", 17, 90, 80, 70, "880000-2222222");
 		
 		System.out.println(s.getName());
 		System.out.println(s.getAge());
